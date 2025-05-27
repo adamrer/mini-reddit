ebd71a88b97c71423ceedd8eea99e53327f83e22
adamrer <adam.rericha@gmail.com>
2025-05-27 17:12:24 +0200

    Update drop script

--- 5-drop_schema.sql
+++ 5-drop_schema.sql
@@ -1,4 +1,12 @@
 
+-- drop views
+drop view view_relevant_posts;
+drop view view_threaded_comments;
+drop view view_most_likeable_users;
+drop view view_most_awarded_users;
+drop view view_communities;
+
+
 -- drop packages
 drop package awards_package;
 drop package comment_awards_package;
@@ -19,7 +27,20 @@ drop sequence communities_id_seq;
 drop sequence posts_id_seq;
 drop sequence users_id_seq;
 
+-- drop indeces
+drop index idx_posts_author_id;
+drop index idx_posts_community_id;
+drop index idx_comments_post_id;
+drop index idx_comments_author_id;
+drop index idx_community_members_community_id;
+drop index idx_community_members_user_id;
 
+-- drop triggers
+drop trigger trg_comment_cycle;
+drop trigger trg_comment_empty;
+drop trigger trg_comment_update_timestamp;
+drop trigger trg_post_empty;
+drop trigger trg_post_update_timestamp;
 
 -- drop tables
 drop table post_awards;
