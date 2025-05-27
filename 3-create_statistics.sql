67aae36237221086ace2a1db15eafa2760909a1f
adamrer <adam.rericha@gmail.com>
2025-05-27 17:08:15 +0200

    Add script for statistics

---
+++ 3-create_statistics.sql
@@ -0,0 +1,16 @@
+-- Author: Adam Øeøicha
+
+begin 
+    dbms_stats.gather_table_stats(user,'awards',cascade => true);
+    dbms_stats.gather_table_stats(user,'comment_awards',cascade => true);
+    dbms_stats.gather_table_stats(user,'comment_votes',cascade => true);
+    dbms_stats.gather_table_stats(user,'comments',cascade => true);
+    dbms_stats.gather_table_stats(user,'communities',cascade => true);
+    dbms_stats.gather_table_stats(user,'community_members',cascade => true);
+    dbms_stats.gather_table_stats(user,'post_awards',cascade => true);
+    dbms_stats.gather_table_stats(user,'post_votes',cascade => true);
+    dbms_stats.gather_table_stats(user,'posts',cascade => true);
+    dbms_stats.gather_table_stats(user,'users',cascade => true);
+end;
+/
+
