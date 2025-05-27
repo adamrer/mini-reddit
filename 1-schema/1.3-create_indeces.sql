9b6e4e39e9095a4298c87371e9ee983c9dc67f75
adamrer <adam.rericha@gmail.com>
2025-05-27 17:11:17 +0200

    Add script for creating indices

---
+++ 1-schema/1.3-create_indeces.sql
@@ -0,0 +1,15 @@
+-- Author: Adam �e�icha
+
+
+-- FOREIGN KEY INDICES
+
+create index idx_posts_author_id on posts(author_id);
+create index idx_posts_community_id on posts(community_id);
+
+create index idx_comments_post_id on comments(post_id);
+create index idx_comments_author_id on comments(author_id);
+
+
+create index idx_community_members_community_id on community_members(community_id);
+create index idx_community_members_user_id on community_members(user_id);
+
