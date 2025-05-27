17182898ef7d09bbd2a3eb2999dc11eec9259948
adamrer <adam.rericha@gmail.com>
2025-05-27 17:08:31 +0200

    Add script for dropping statistics

---
+++ 4-drop_statistics.sql
@@ -0,0 +1,18 @@
+-- Author: Adam Øeøicha
+
+
+begin
+
+    dbms_stats.delete_table_stats(user, 'awards');
+    dbms_stats.delete_table_stats(user, 'comment_awards');
+    dbms_stats.delete_table_stats(user, 'comment_votes');
+    dbms_stats.delete_table_stats(user, 'comments');
+    dbms_stats.delete_table_stats(user, 'communities');
+    dbms_stats.delete_table_stats(user, 'community_members');
+    dbms_stats.delete_table_stats(user, 'post_awards');
+    dbms_stats.delete_table_stats(user, 'post_votes');
+    dbms_stats.delete_table_stats(user, 'posts');
+    dbms_stats.delete_table_stats(user, 'users');
+
+end;
+/
