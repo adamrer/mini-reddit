2af2101bd2ff5369bdfa8ce9f03ed4e88a4ca942
adamrer <adam.rericha@gmail.com>
2025-05-29 11:11:04 +0200

    Add indices for timestamps
    For columns created_at in tables posts and comments

--- 1-schema/1.3-create_indices.sql
+++ 1-schema/1.3-create_indices.sql
@@ -1,15 +1,17 @@
 -- Author: Adam �e�icha
 
 
--- FOREIGN KEY INDICES
-
+-- POSTS
 create index idx_posts_author_id on posts(author_id);
 create index idx_posts_community_id on posts(community_id);
+create index idx_posts_created_at on posts(created_at);
 
+-- COMMENTS
 create index idx_comments_post_id on comments(post_id);
 create index idx_comments_author_id on comments(author_id);
+create index idx_comments_created_at on comments(created_at);
 
-
+-- COMMUNITY MEMBERS
 create index idx_community_members_community_id on community_members(community_id);
 create index idx_community_members_user_id on community_members(user_id);
 
